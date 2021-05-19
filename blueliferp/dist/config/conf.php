49b2e6f6c90a07f8cf3a87b4d0dbcd028bbe88fc
Niclas K <66620423+Niclas-Kedzierski@users.noreply.github.com>
2021-05-20 00:11:42 +0200

    Create conf.php

---
+++ blueliferp/dist/config/conf.php
@@ -0,0 +1,10 @@
+<?php
+    // Globale Variable
+    $page;
+    $category;
+    $ausbildung_new=0;
+
+    // Statische Variable
+    $servername = "BlueLifeRP";
+    $version    = "0.1.0-BETA";
+?>
